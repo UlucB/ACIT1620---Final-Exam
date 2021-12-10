cf3381852c71eda52f0e5c29114190ee30b2ce94
Uluc <ubulbirol@yahoo.com>
2021-12-10 12:48:24 -0800

    Working on Q3

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,5 @@
 
+// Question 1: Dark Mode
 
 document.querySelector(".toggle").addEventListener("click", event =>{
     console.log(event)
@@ -6,9 +7,21 @@ document.querySelector(".toggle").addEventListener("click", event =>{
     if (!body.classList.contains('light-theme')) {
         body.classList.add('light-theme');
         body.classList.remove('dark-theme');
-
 }   else {
         body.classList.add('dark-theme');
         body.classList.remove('light-theme');
 }
+})
+
+// Question 3: Contact Form
+
+document.querySelector('#frm-btn').addEventListener('click', event => {
+    let name = document.querySelector('#name').value
+    let email = document.querySelector('#email').value
+    let message = document.querySelector('#msg').value
+
+    console.log('name:', name, '\nemail:', email, '\nmessage', message)
+
+
+
 })
